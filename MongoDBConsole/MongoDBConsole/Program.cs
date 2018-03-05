2594ee4bce513225808ee24bba4141fa47502993
Quentin Da Silva <32438087+Sh0ri@users.noreply.github.com>
2018-03-05 21:21:28 +0100

    Initial commit
    Small programm to convert json file to bson file + mongodb connection and insert

---
+++ MongoDBConsole/MongoDBConsole/Program.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+
+            Console.WriteLine("test");
+
+            Console.ReadLine();
+        }
+
+        static BsonDocument[] GetValues()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"permits_ottawa.json");
+            BsonDocument[] Bsonlines = new BsonDocument[lines.Length];
+
+            int compt = 0;
+            foreach(string line in lines)
+            {
+                Bsonlines[compt] = BsonDocument.Parse(line);
+            }
+
+            return Bsonlines;
+        }
+
+        static async Task MainAsync()
+        {
+            //BsonDocument.Parse(json)
+
+            var connectionString = "mongodb://localhost:27017";
+
+            var client = new MongoClient(connectionString);
+
+            IMongoDatabase db = client.GetDatabase("school");
+
+
+            IMongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("students");
+
+
+            /////////////////////////////////////////////// Insert Databases Values /////////////////////////////////////////////////////////////////////////
+
+            BsonDocument[] Bsonlines = GetValues();
+
+            foreach(BsonDocument data in Bsonlines)
+            {
+                Console.WriteLine(data.ToString());
+                await collection.InsertOneAsync(data);
+            }
+
+        }
+    
+    }
+}
